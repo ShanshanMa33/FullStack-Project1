5550e2eb142f49032e555c0f734b47c8309ebeb3
yvonne nuo <yvonnenuo@yvonnedeMacBook-Pro.local>
2026-01-26 21:11:09 -0800

    update backend: sync local state

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-// 1. Import the Controller (The Brain)
-// This will import an object containing all our logic functions
+// 1. Import the Controller
 const productController = require('../controllers/productController');
 
-// 2. Import the Validator (The Gatekeeper)
-// We use { destructuring } because we used 'exports.validateProduct' in the middleware file
+// 2. Import the Validator
 const { validateProduct } = require('../middlewares/productValidator');
 
 // --- ROUTES ---
-
-/**
- * @route   GET /api/products
- * @desc    Fetch all products from the JSON database
- * @access  Public
- */
 router.get('/products', productController.getAllProducts);
 
-/**
- * @route   POST /api/products
- * @desc    Validate data and create a new product
- * @access  Admin (Simulated)
- */
 router.post('/products', validateProduct, productController.createProduct);
 
 router.delete('/products/:id', productController.deleteProduct);
--- backend/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-
-router.get("/", (req, res) => {
-    res.json({ message: "..." }) // message is currently a place holder
-})
-
-module.exports = router;
